8ac907f7a2df05924ed3bafbe5dff67886cc58e9
Nicolas Kuhn <nicolaskuhn02@gmail.com>
2021-08-21 20:46:26 -0300

    estilos generales añadidos

---
+++ src/css/globalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+
+body {
+    font-size: 14px;
+    height: 100%;
+    font-family: 'Nunito Sans', sans-serif;
+}
+`;
---
+++ src/css/themes.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  text: "#111517",
+  input: "#858585",
+  background: "#fafafa",
+  elements: "#ffffff",
+};
+
+export const darkTheme = {
+  text: "#ffffff",
+  input: "#858585",
+  background: "#202c37",
+  elements: "#2b3945",
+};
